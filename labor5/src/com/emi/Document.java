09d138c1d9ab22a3494422dd8a4bf5e6bc9b6a8f
emilian955 <emilian955@gmail.com>
2020-03-19 15:42:20 +0200

    Document
    settere si gettere pentru atributele din clasa

--- labor5/src/com/emi/Document.java
+++ labor5/src/com/emi/Document.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Document implements Serializable {
-    private String id;
+    public String id;
     private String name;
     private String location; //file name or Web page
      public Document(String id_,String name_, String location_) {
@@ -14,10 +14,58 @@ public class Document implements Serializable {
          this.location=location_;
      }
     private Map<String, Object> tags = new HashMap<>();
-    //…
-    public void addTag(String key, Object obj) {
+    public String getName() {
+        return name;
+    }
+    public String getId() {
+        return id;
+    }
+
+
+
+    public String getLocation()
+    {
+        return location;
+    }
+    public void setLocation(String location)
+    {
+        this.location = location;
+    }
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public void addTag(String key, Object obj)
+    {
         tags.put(key, obj);
     }
-//…
-}
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Document{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", tags=" + tags +
+                '}';
+    }
+
+
+    public void setTags(Map<String, Object> tags)
+    {
+        this.tags = tags;
+    }
+    public Map<String, Object> getTags()
+    {
+        return tags;
+    }
 }
