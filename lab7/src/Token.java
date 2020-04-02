6c0f407140b1e71b0efd34b746d95665f019ac28
emilian955 <emilian955@gmail.com>
2020-04-02 23:34:21 +0300

    Token class
    settere
    gettere
    constructor

--- lab7/src/Token.java
+++ lab7/src/Token.java
@@ -1,2 +1,23 @@
 public class Token {
+    private int val;
+    public void setVal(int val){
+        this.val=val;
+    }
+    public int getVal()
+    {
+        return this.val;
+    }
+    public Token(int val)
+    {
+        if(val<0)
+            this.val=-1;
+        else
+            this.val=val;
+    }
+    @Override
+    public String toString() {
+        return "Token{" +
+                "val=" + val +
+                '}';
+    }
 }
