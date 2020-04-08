5ca8a8794709566c48a2a73365e1228971202209
emilian955 <emilian955@gmail.com>
2020-04-08 22:39:51 +0300

    Mihaila
    settere gettere la ambele clase

--- Lab8/src/com/emi/AlbumController.java
+++ Lab8/src/com/emi/AlbumController.java
@@ -1,4 +1,21 @@
 package com.emi;
 
 public class AlbumController {
+    String name;
+    public int artistId;
+    public int releaseYear;
+    public void setname(String name_) {
+        this.name = name_;
+    }
+    public String getname() {
+        return this.name;
+    }
+    public void setartistId(int artistId_) {
+        this.artistId = artistId_;
+    }
+    public int getartistId() {
+        return this.artistId;
+    }
+    public void setreleaseYear(int releaseYear_) {this.releaseYear = releaseYear_; }
+    public int getreleaseYear() { return this.releaseYear; }
 }
--- Lab8/src/com/emi/ArtistController.java
+++ Lab8/src/com/emi/ArtistController.java
@@ -1,3 +1,13 @@
 package com.emi;
 public class ArtistController {
+    String name;
+    String country;
+    public void setname(String name_) {
+        this.name = name_;
+    }
+    public void setcountry(String country_){this.country=country_;}
+    public String getname() {
+        return this.name;
+    }
+    public String getcountry(){return this.country;}
 }
