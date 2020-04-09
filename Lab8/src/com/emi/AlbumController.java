2f9f8d7edab8dea32af81a10da198295f99a6d87
emilian955 <emilian955@gmail.com>
2020-04-09 13:24:09 +0300

    Mihaila Emilian
    Functia find by name si find by artist. cautand in tabele dupa cheile de cautare specfice

--- Lab8/src/com/emi/AlbumController.java
+++ Lab8/src/com/emi/AlbumController.java
@@ -1,7 +1,13 @@
 package com.emi;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AlbumController {
     String name;
+    Connection con;
     public int artistId;
     public int releaseYear;
     public void setname(String name_) {
@@ -18,4 +24,14 @@ public class AlbumController {
     }
     public void setreleaseYear(int releaseYear_) {this.releaseYear = releaseYear_; }
     public int getreleaseYear() { return this.releaseYear; }
+    public void create(String name, int artistId, int releaseYear){
+
+    }
+    public void findByArtist(int artistId) throws SQLException {
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery("select * from albums where artist_id="+this.artistId);
+        while(rs.next())
+            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+        con.close();
+    }
 }
--- Lab8/src/com/emi/ArtistController.java
+++ Lab8/src/com/emi/ArtistController.java
@@ -1,7 +1,14 @@
 package com.emi;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ArtistController {
     String name;
     String country;
+    Connection con;
     public void setname(String name_) {
         this.name = name_;
     }
@@ -10,4 +17,14 @@ public class ArtistController {
         return this.name;
     }
     public String getcountry(){return this.country;}
+    public void create(String name, String country){
+
+    }
+    public void findByName(String name) throws SQLException {
+        Statement stmt=con.createStatement();
+        ResultSet rs=stmt.executeQuery("select * from artists where name="+this.name);
+        while(rs.next())
+            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));
+        con.close();
+    }
 }
